d77555e4c8dc3b1e0ebc44c92d303f06a0fea3f6
david <david@hamlyn>
2021-06-06 19:34:41 +0100

    Attempt to make remote errors work as expected

--- PSano/private/PSanoFileInSession.ps1
+++ PSano/private/PSanoFileInSession.ps1
@@ -19,11 +19,26 @@ class PSanoFileInSession : PSanoFile {
 
     # we need to do the same as the local class but in a remote session
     [string[]] readFileContents() {
+        <#
+        ICM does not pass stop errors back to this session, we need to 
+        capture any errors and throw them in this session.        
+        #>
+        $RemoteErrors = @()
+        Clear-Variable -Force -Name RemoteErrors -ErrorAction Ignore
         $Content = Invoke-Command -Session $this.Session -ScriptBlock {
             Param($Path)
             # we are just going to support litterals for now.
             Get-Content -LiteralPath $path
-        } -ArgumentList $this.FullPath
+        } -ArgumentList $this.FullPath -ErrorVariable RemoteErrors -ErrorAction SilentlyContinue
+
+        if ($RemoteErrors.count -gt 0) {
+            #errors found
+            # ignore file not found errors
+            if ($RemoteErrors[0].categoryinfo.category -notlike 'ObjectNotFound'){
+                throw $RemoteErrors[0] # can only throw one error.
+            }
+        }
+        # empty doc if files does not exist
         if ($content.count -eq 0){
             $Content = [string[]]""
         }
