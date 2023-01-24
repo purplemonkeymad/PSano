1991d77b441689bbf9c164037dd1d7a031e73062
david <david@hamlyn>
2023-01-24 20:11:44 +0000

    Check local file then system installed libraries

--- PSano/ScriptsToProcess/LoadAssembelies.ps1
+++ PSano/ScriptsToProcess/LoadAssembelies.ps1
@@ -1,4 +1,28 @@
+# this runs in caller context so plan is no variables
 
-        Add-Type -Path $env:LOCALAPPDATA\PackageManagement\NuGet\Packages\System.ValueTuple.4.5.0\lib\netstandard1.0\System.ValueTuple.dll
-        Add-Type -Path $env:LOCALAPPDATA\PackageManagement\NuGet\Packages\NStack.Core.1.0.7\lib\netstandard2.0\NStack.dll
-        Add-Type -Path $env:LOCALAPPDATA\PackageManagement\NuGet\Packages\Terminal.Gui.1.9.0\lib\netstandard2.0\Terminal.Gui.dll
+@(
+        [pscustomobject]@{File = 'lib\netstandard1.0\System.ValueTuple.dll'; Package = 'System.ValueTuple'}
+        [pscustomobject]@{File = 'lib\netstandard2.0\NStack.dll'; Package = 'NStack.Core'}
+        [pscustomobject]@{File = 'lib\netstandard2.0\Terminal.Gui.dll'; Package = 'Terminal.Gui'}
+) | ForEach-Object {
+        # nested join-path as 5.1 does not support additional child path param
+        try{ 
+                Add-Type -Path (Join-Path $PSScriptRoot (Join-Path ".." $_.File ) ) -ErrorAction Stop
+                return #sucess
+        } catch {
+                Write-Warning "Import of library $($_.TargetObject) failed. Looking for installed packages instead."
+        }
+        Get-Package $_.Package | 
+                Add-Member -MemberType NoteProperty -Name file -Value $_.File -PassThru |
+                Sort-Object Version -Descending | 
+                Select-Object -First 1 | ForEach-Object {
+
+                # package is installed so we can use that instead.
+                Join-Path -Path (
+                        Split-Path $_.Source -Parent
+                ) -ChildPath $_.File
+        } | ForEach-Object {
+                Add-Type -Path $_ -ErrorAction Stop
+        }
+
+}
